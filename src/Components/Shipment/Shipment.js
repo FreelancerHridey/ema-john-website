c32bc6d9b80a39865b93244d98092d98ae7a1323
FreelancerHridey <76817261+FreelancerHridey@users.noreply.github.com>
2021-02-14 04:14:17 +0600

    shipping section done

--- src/Components/Shipment/Shipment.js
+++ src/Components/Shipment/Shipment.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { UserContext } from '../../App';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,14 +16,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 const Shipment = () => {
     const classes = useStyles();
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
     return (
         <div  className={classes.root}>
             <form styles={{marginLeft: "100px" }} className={classes.root} noValidate autoComplete="off">
-                <TextField id="standard-basic" label="Enter your name" />
-                <TextField id="standard-basic" label="Enter email address" />
+                <TextField id="standard-basic" defaultValue={loggedInUser.name} label="Enter your name" />
+                <TextField id="standard-basic" defaultValue={loggedInUser.email} label="Enter your email address" />
                 <TextField id="standard-basic" label="Enter phone number" />
                 <Button variant="contained" color="primary">Submit</Button>
             </form>
